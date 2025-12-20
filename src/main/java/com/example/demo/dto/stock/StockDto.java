17788137e5222701483e59236790f9811eba5996
taingsiveminh <mrminhzin@gmail.com>
2025-12-20 17:35:50 +0700

    refactor(stock): rename from model to dto

--- src/main/java/com/example/demo/dto/stock/StockDto.java
+++ src/main/java/com/example/demo/dto/stock/StockDto.java
@@ -1,9 +1,9 @@
-package com.example.demo.model.stock;
+package com.example.demo.dto.stock;
 
 import lombok.Data;
 
 @Data
-public class StockModel {
+public class StockDto {
     private Long productId;
     private Integer quantity;
 }
